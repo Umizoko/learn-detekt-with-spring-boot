92b33fe24d47f3e032753931d545c7e1330b04c5
Maruyama Kai <0123mark3210@gmail.com>
2022-05-15 23:14:27 +0900

    add github actions

--- src/main/kotlin/com/example/demo/domain/talent/TalentName.kt
+++ src/main/kotlin/com/example/demo/domain/talent/TalentName.kt
@@ -1,6 +1,6 @@
 package com.example.demo.domain.talent
 
-adata class TalentName(val value: String) {
+data class TalentName(val value: String) {
     init {
         if (value.length > 255) {
             throw Exception("名前は255文字以下で入力してください")
